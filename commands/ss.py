53d418c658d492225dfe7d44d38b578386423a73
Rizen <123223112+Richard0070@users.noreply.github.com>
2024-10-13 13:58:50 +0530

    Update ss.py

--- commands/ss.py
+++ commands/ss.py
@@ -8,20 +8,26 @@ class Screenshot(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="screenshot", aliases=["ss"], usage="<url>", description="Take screenshot of a website.")
+    @commands.command(name="screenshot", aliases=["ss"], usage="<url>", description="Take a screenshot of a website.")
     @commands.has_permissions(administrator=True)
     async def screenshot(self, ctx, url: str):
         m = await ctx.send('Taking screenshot...')
         async with async_playwright() as p:
-            browser = await p.chromium.launch()
+            # Launch the browser with the necessary arguments
+            browser = await p.chromium.launch(args=['--disable-web-security'])
             page = await browser.new_page()
             await page.goto(url)
-            screenshot_path = f'screenshot_{ctx.message.id}.png'
+
+            # Optional: Wait for a few seconds to ensure the page is fully loaded
             await asyncio.sleep(3)
+
+            # Take the screenshot
+            screenshot_path = f'screenshot_{ctx.message.id}.png'
             await page.screenshot(path=screenshot_path)
             await browser.close()        
             await m.delete()
 
+            # Send the screenshot back to Discord
             await ctx.send(file=discord.File(screenshot_path))
             os.remove(screenshot_path)
 
